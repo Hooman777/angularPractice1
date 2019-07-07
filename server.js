461d3464ab5d8b83fea5436214196333434d2b9e
hooman777 <eng.hesamyan@gmail.com>
2019-07-08 02:00:59 +0400

    using build

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // // Serve only the static files form the dist directory
-// app.use(express.static("./dist/dotnet-intermediate-workshop"));
+app.use(express.static("./dist/practice1"));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 80);
